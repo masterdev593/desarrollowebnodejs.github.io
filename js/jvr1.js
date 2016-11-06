8fc57ea9215a79c94d8ede8d0db9da2ea286e1eb
Vladimir Rodríguez <fizzvr@gmail.com>
2016-11-06 17:50:12 -0500

    Finally update navbar
    :lock:
    
    Desarrollo Web NodeJS • Quito Ecuador
    
    [Fix: X]

--- js/jvr1.js
+++ js/jvr1.js
@@ -1,26 +1,14 @@
   $(document).ready(function() {
             init();
-            console.log("init");
         });
-
         function init()
         {
-
             $('#navbar-collapse ul li a').click(function() {
                 $('.navbar-toggler:visible').click();
             });
-
-
-        }
-        function navColapso()
-        {
-            //jQuery to collapse the navbar on scroll
             $(window).scroll(function() {
-                 if ($(".navbar").offset().top > 50)     {
-                    $(".navbar-fixed-top").addClass("top-nav-collapse");
-                } else {
-                    $(".navbar-fixed-top").removeClass("top-nav-collapse");
-                }
+                 if ($(".navbar-toggler").offset().top > 40) {
+                    $("#navbar-collapse").removeClass("in");
+                } 
             });
         }
-      
